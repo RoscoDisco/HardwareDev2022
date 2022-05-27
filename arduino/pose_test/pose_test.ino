9613a1e20a97a76ce786e7e4972d28532cd90502
RoscoDisco <ross.henry@kcl.ac.uk>
2022-05-27 14:06:22 +0100

    arduino temp

--- arduino/pose_test/pose_test.ino
+++ arduino/pose_test/pose_test.ino
@@ -1,46 +1,33 @@
-/* 
+/*
  * rosserial::geometry_msgs::PoseArray Test
- * Sums an array, publishes sum 
+ * Sums an array, publishes sum
  */
 
+// INclude at start
 #include <ros.h>
 #include <geometry_msgs/PoseStamped.h>
 
 
 ros::NodeHandle nh;
 
-
-bool set_; 
-
-
-geometry_msgs::Pose sum_msg;
-ros::Publisher p("sum", &sum_msg);
-
 void messageCb(const geometry_msgs::PoseStamped& msg){
-  sum_msg.position.x = 0;
-  sum_msg.position.y = 0;
-  sum_msg.position.z = 0;
+  x = msg.pose.position.x;
+  y = msg.pose.position.y;
 
-  sum_msg.position.x += msg.pose.position.x;
-  sum_msg.position.y += msg.pose.position.y;
-  sum_msg.position.z += msg.pose.position.z;
- 
-  digitalWrite(13, HIGH-digitalRead(13));   // blink the led
+  return;
 }
 
 ros::Subscriber<geometry_msgs::PoseStamped> s("haptic/updated_pose",messageCb);
 
 void setup()
-{ 
-  pinMode(13, OUTPUT);
+{
+  // ROS setup
   nh.initNode();
   nh.subscribe(s);
-  nh.advertise(p);
 }
 
 void loop()
-{  
-  p.publish(&sum_msg);
+{
   nh.spinOnce();
-  delay(10);
+
 }
